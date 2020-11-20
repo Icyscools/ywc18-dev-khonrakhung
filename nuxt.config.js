3fdcbcc7c8663dec11028d6d577b2abc1959cd95
Icyscools <meet.ice1@gmail.com>
2020-11-21 06:01:08 +0700

    :zap: Add layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,20 +5,22 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Prompt',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -41,6 +43,5 @@ export default {
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {},
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,63 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ywc18-dev-khonrakhung
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="app">
+    <div class="bar">
+      <div class="container">Navbar</div>
+    </div>
+    <div class="bar">
+      <div class="container">History</div>
+    </div>
+    <div class="main">
+      <div class="hidden w-1/2 lg:w-1/4 md:block min-h-full">
+        <div class="rounded border border-gray-500 mr-3 p-3" style="height: 1000px">
+          <FilterStoreBar />
+        </div>
+      </div>
+      <div class="content-bar">
+        <RenderStoreCard />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  //need fetching
+  //https://panjs.com/ywc18.json
+};
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
+/* Sample `apply` at-rules with Tailwind CSS */
+.app {
+  @apply min-h-screen;
+  font-family: 'Prompt', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+    'Noto Color Emoji' !important;
 }
-*/
+
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  @apply flex justify-start items-center;
+}
+
+.bar {
+  @apply flex justify-center items-center w-full bg-red-200 mx-auto;
+}
+
+.main {
+  @apply flex justify-start m-3;
+}
+
+.sidebar {
+  @apply w-1/4;
+}
+
+.content-bar {
+  @apply flex-1;
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
+    Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
